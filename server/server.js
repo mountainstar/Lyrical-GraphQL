74510dc23d1ad809aa0ac7f8e9a787a262619074
Chris Wirth <chris@goozmo.com>
2017-11-24 17:36:39 -0500

    songs generator

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://chriswirth:Ph53n4x420@ds251845.mlab.com:51845/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
